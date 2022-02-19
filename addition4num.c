ca885553c7c70c8824d1f3928ce9fbdf8cb279b1
KP20121995 <99796274+KP20121995@users.noreply.github.com>
2022-02-19 20:00:22 +0530

    Delete addition4num.c

--- addition4num.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main()
-{
-    int num1,num2,num3,num4,addition;
-    scanf("%d%d%d%d",&num1,&num2,&num3,&num4);
-    addition=num1+num2+num3+num4;
-    printf("addition of num1 num2 num3 num4  %d ",addition);
-    return 0;
-}
