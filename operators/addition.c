c6b499e7d8a4c4f322035d058ba2c3205cf68f26
KP20121995 <99796274+KP20121995@users.noreply.github.com>
2022-02-19 19:42:00 +0530

    all operators codes

---
+++ operators/addition.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+int main ()
+{
+   float num1,num2,addition;
+   printf("the addition of num1 and num2");
+   scanf("%f%f",&num1,&num2);
+   addition=num1+num2;
+   printf(" addition=%f",addition);
+   getch();
+   return 0 ;
+   }
---
+++ operators/assignment4.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+int main ()
+{
+   float num1=90,division;
+   printf("the value of num1 ");
+   division=num1/30;
+   printf(" division=%f \n",division);
+   getch();
+   return 0 ;
+   }
